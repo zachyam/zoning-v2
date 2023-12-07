f918ff921848d4ad69934818703dbb46a83091cd
Zach Yam <zyam@zyam-mn2.linkedin.biz>
2023-12-07 13:06:46 -0800

    install

--- src/components/AddNewRegulation.js
+++ src/components/AddNewRegulation.js
@@ -56,6 +56,11 @@ export default function AddNewRegulation({ zone, newCodeRegulationName, setNewCo
             },
             body: JSON.stringify({ data }),
             });
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+            response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
 
             const responseData = await response.json();
             console.log(responseData);
