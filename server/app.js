1d549b943089702d911acb9e374aed0b1f9174db
Zach Yam <zyam@zyam-mn2.linkedin.biz>
2023-12-07 03:56:03 -0800

    install

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const pool = require('./database')
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res, next) => {
     res.status(200).json({ success: 'Hello Server' });
